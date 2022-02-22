c2a87424cb764a230112c65cb070d18621bc1776
kei-sawa <keiki.sawada@gmail.com>
2022-02-22 17:05:17 +0900

    initial commit

---
+++ subtotal.js
@@ -0,0 +1,8 @@
+function subtotal(price, lot){
+  var subtotal = price * lot;
+  return subtotal;
+}
+
+// function changeLot() { 
+//   console.log(parseInt(document.getElementById("orderLot").value));
+// }
